8c4d248200765b273465048dbcc8f0843219993a
alo5s <angelgonzalez.ag00@gmail.com>
2023-06-20 00:35:32 -0300

    config-3

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY =  os.environ.get('SECRET_KET_FLASK')
     #localhost:8000
     #SERVER_NAME = "localhost:8000"
-    SERVER_NAME = "127.0.0.1:8000"
+    #SERVER_NAME = "127.0.0.1:8000"
     DEBUG = True
     
     TEMPLATE_FOLDER = "views/templates/"
